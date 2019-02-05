f40b771793eb4357aad2b8a85e87f095b4c3cca6
Santosh Suresh <santosh.deshpande@gmail.com>
2019-02-05 12:27:57 +0530

    Complete chapter#3 static_pages

--- lib/sample_app_web/controllers/page_controller.ex
+++ lib/sample_app_web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule SampleAppWeb.PageController do
   use SampleAppWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def about(conn, _params) do
+    render(conn, "about.html")
+  end
+
+  def help(conn, _params) do
+    render(conn, "help.html")
+  end
+
+  def home(conn, _params) do
+    render(conn, "home.html")
   end
 end
